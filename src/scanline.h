8c3e10c85811336b686d6dd8123ecff33776357d
Alexandre Kaspar <xion.luhnis@gmail.com>
2014-11-15 13:37:33 -0500

    RowMajor parameter

--- src/scanline.h
+++ src/scanline.h
@@ -35,9 +35,17 @@ void scanline(Grid &grid, unsigned int numIters){
     for(unsigned int iter = 0; iter < numIters; ++iter){
         bool done = true;
         // scanline traversal
-        for(auto i : grid.reversed(rev)){
+		iterator it, end;
+		if(!rev){
+			it = grid.begin();
+			end = grid.end();
+		} else {
+			it = grid.rbegin();
+			end = grid.rend();
+		}
+        for(; it != end; ++it){
             // filter index
-            if(IterationFilter(i)) continue;
+            if(IterationFilter(it)) continue;
             // execute improvement
             if(Algorithm(i)){
                done = false; // the update was successful => more to do
@@ -52,10 +60,14 @@ void scanline(Grid &grid, unsigned int numIters){
     }
 }
 
+template < typename Index, bool RowMajor = true >
 struct Iterator2D {
 	
-	explicit Iterator2D(int s, int d0, int d1, int x0, int x1)
-	: i0(x0), i1(x1), step(s), dir0(d0), dir1(d1) {
+	typedef Iterator<Index, RowMajor> this_type;
+	typedef Index this_value;
+	
+	explicit Iterator2D(int x0, int x1, int s0, int e0, int d0, int d1)
+	: i0(x0), i1(x1), start(s0), end(e0), dir0(d0), dir1(d1) {
 	}
 	
 	inline int x0() const {
@@ -65,43 +77,70 @@ struct Iterator2D {
 		return i1;
 	}
 	
-	inline Iterator2D & operator ++() {
+	inline this_type & operator ++() {
 		i0 += dir0;
-		if(i0 == step){
+		if(i0 == end){
 			i0 = start;
 			i1 += dir1;
 		}
 		return *this;
 	}
 	
-	inline bool operator==(const Iterator2D &it) const {
+	inline bool operator==(const this_type &it) const {
 		return it.i0 == i0 && it.i1 == i1;
 	}
 	
-	template <typename T>
-	T as() const {
-		return T(i0, i1);
+	operator this_value() const {
+		return RowMajor ? this_value(i0, i1) : this_value(i1, i0);
 	}
+	
+	inline this_value operator*() const {
+		return this_value(*this);
+	}
+	
 private:
 	int i0, i1;
-	int start;
-	int step;
+	int start, end;
 	int dir0, dir1;
 };
 
-template < bool RowMajor >
+template < typename Index, bool RowMajor >
 struct Grid2D {
-	typedef Iterator2D index;
-	typedef Iterator2D iterator;
-	typedef Iterator2D const_iterator;
-	typedef Iterator2D reverse_iterator;
+	typedef Index index;
+	typedef Iterator2D<Index, RowMajor> iterator;
+	typedef iterator const_iterator;
+	typedef iterator reverse_iterator;
 	
-	Grid2D(int s0, int s1) : size0(s0), size1(s1){}
+	Grid2D(int s0, int s1) : size0(RowMajor ? s0 : s1), size1(RowMajor ? s1 : s0){}
 	
-	const_iterator begin() const;
-	const_iterator end() const;
-	iterator rbegin() const;
-	iterator rend() const;
+	iterator begin() const {
+		return iterator(
+			0, 0,
+			0, size0,
+			1, 1
+		);
+	}
+	iterator end() const {
+		return iterator(
+			0, size1, 
+			0, size0,
+			1, 1
+		);
+	}
+	iterator rbegin() const {
+		return iterator(
+			size0 - 1, size1 - 1, 
+			size0 - 1, -1, 
+			-1, -1
+		);
+	}
+	iterator rend() const {
+		return iterator(
+			size0 - 1, -1,
+			size0 - 1, -1
+			-1, -1
+		);
+	}
 	
 private:
 	int size0, size1;
